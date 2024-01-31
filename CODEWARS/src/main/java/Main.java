8b526370aabb8eb9e348947dfcfa88f7db671415
Венера <125314304+VeneraChepok@users.noreply.github.com>
2024-01-31 19:52:33 +0300

    and

---
+++ CODEWARS/src/main/java/Main.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] ints = {5, 8, 6, 3, 4};
+
+        System.out.println(Arrays.toString(Test1.sortArray(ints)));
+
+//        System.out.println(Test1.filterList(List.of(1, 2, "a", "b",2,30,"bhhh")));
+//        System.out.println(Test1.isIsogram("Dermatoglyphics"));
+//        System.out.println(Test1.isIsogram("moose"));
+//        System.out.println(Test1.persistence(12));
+        //       System.out.println(Test1.digital_root(493193));
+//        List<Integer> list = new ArrayList<>();
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//        list.add(4);
+//        list.add(5);
+//        for (int i = 0; i < list.size(); i++) {
+//            list.remove(i);
+//        }
+//        System.out.println(list);
+
+//
+//
+//        for(int i = 0; i < 1000; i++) {
+//            boolean enabled = true;
+//            if (i % 3 != 0) {
+//                enabled = false;
+//            }
+//
+//            if (i % 5 == 0) {
+//                enabled = false;
+//            }
+//
+//            if (!testSum(i)) {
+//                enabled = false;
+//            }
+//
+//            if (enabled == true) {
+//                System.out.println("Число: " + i);
+//            }
+//        }
+//    }
+//
+//    public static boolean testSum(int in) {
+//        int res = 0;
+//        while (in > 0) {
+//            res += in % 10;
+//            in = in / 10;
+//        }
+//        return res < 10;
+    }
+}
+
---
+++ Sprint_4/src/main/java/model/Subtask.java
@@ -0,0 +1,23 @@
+package model;
+
+import model.Epic;
+
+public class Subtask extends Task {
+    private Epic epic;
+
+
+    public Subtask(String name, String status, String description, Epic epic) {
+        super(name, status, description);
+        this.epic = epic;
+    }
+
+    @Override
+    public Epic getEpic() {
+        return epic;
+    }
+
+    public void setEpic(Epic epic) {
+        this.epic = epic;
+    }
+    
+}
